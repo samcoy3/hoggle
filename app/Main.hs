c0d65d77e9515211b585c5c23faed6a938951c22
Sam Coy <samcoy3@gmail.com>
2020-06-06 15:54:25 +0100

    Tweaked some of the random generation

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,8 @@ module Main where
 import Boggle.Board
 import Boggle.Scoring
 
+import Web.API
+import Web.Types
+
 main :: IO ()
 main = undefined
--- src/Boggle/Board.hs
+++ src/Boggle/Board.hs
@@ -2,18 +2,12 @@ module Boggle.Board
  (randomBoard,
   neighbours,
   Point,
-  Board) where
+  Board)
+where
 
 import qualified Data.Map as M
 
-import Control.Monad
-import Control.Monad.Primitive
-
-import System.Random.MWC as MWC
-import System.Random.MWC.Distributions as Dist
-
-import qualified Data.Vector as V
-import Data.Vector (Vector)
+import System.Random
 
 type Point = (Int, Int)
 type Tile = String
@@ -22,43 +16,55 @@ type Board = M.Map Point Tile
 neighbours :: Point -> [Point]
 neighbours (x, y) = [(x', y') | x' <- [x-1..x+1], y' <- [y-1..y+1], (x, y) /= (x', y')]
 
-tileBag :: Vector Tile
-tileBag = V.fromList .
-  concat .
-  (map (\(l, f) -> replicate f l)) $
-  [
-    ("A", 9),
-    ("B", 2),
-    ("C", 2),
-    ("D", 4),
-    ("E", 12),
-    ("F", 2),
-    ("G", 3),
-    ("H", 2),
-    ("I", 9),
-    ("J", 1),
-    ("K", 1),
-    ("L", 4),
-    ("M", 2),
-    ("N", 6),
-    ("O", 8),
-    ("P", 2),
-    ("QU", 1),
-    ("R", 6),
-    ("S", 4),
-    ("T", 6),
-    ("U", 4),
-    ("V", 2),
-    ("W", 2),
-    ("X", 1),
-    ("Y", 2),
-    ("Z", 1)
-  ]
+tileBag :: [Tile]
+tileBag =
+  concat
+    . (map (\(l, f) -> replicate f l))
+    $ [ ("A", 9),
+        ("B", 2),
+        ("C", 2),
+        ("D", 4),
+        ("E", 12),
+        ("F", 2),
+        ("G", 3),
+        ("H", 2),
+        ("I", 9),
+        ("J", 1),
+        ("K", 1),
+        ("L", 4),
+        ("M", 2),
+        ("N", 6),
+        ("O", 8),
+        ("P", 2),
+        ("QU", 1),
+        ("R", 6),
+        ("S", 4),
+        ("T", 6),
+        ("U", 4),
+        ("V", 2),
+        ("W", 2),
+        ("X", 1),
+        ("Y", 2),
+        ("Z", 1)
+      ]
 
-randomBoard :: Int -> Gen RealWorld -> IO Board
-randomBoard size rng = do
-  tiles <- randomTiles rng
-  return . M.fromList . zip [(a, b) | a <- [1..size], b <- [1..size]] $ V.toList tiles
+randomBoard :: Int -> IO Board
+randomBoard size = do
+  tiles <- randomTiles
+  return . M.fromList . zip [(a, b) | a <- [1 .. size], b <- [1 .. size]] $ tiles
 
-randomTiles :: Gen RealWorld -> IO (Vector Tile)
-randomTiles gen = uniformShuffle tileBag gen
+randomTiles :: IO [Tile]
+randomTiles = shuffleList tileBag
+
+shuffleList :: [a] -> IO [a]
+shuffleList [] = return []
+shuffleList xs = do
+  (randomOne, remaining) <- randomFromList xs
+  shuffledRest <- shuffleList remaining
+  return $ randomOne : shuffledRest
+
+randomFromList :: [a] -> IO (a, [a])
+randomFromList [] = error "Cant' sample from empty list."
+randomFromList l = do
+  index <- randomRIO (0, length l - 1)
+  return (l !! index, take index l ++ drop (index + 1) l)
